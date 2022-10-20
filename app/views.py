c5309af9cfeed91d9c1adf0a52267e59fb028df4
keyandu <81390951+keyandu@users.noreply.github.com>
2022-10-20 14:47:33 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the app page.")
+    
+def get_class(request):
+    url = "http://luthers-list.herokuapp.com/api/dept/CS/"
+    response = requests.get(url).json()
+    return render(request,'app/classinfo.html',{'response':response})
