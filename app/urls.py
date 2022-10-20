3a2904f55cac6bbe0bc742dde19bf6168fe3da6e
keyandu <81390951+keyandu@users.noreply.github.com>
2022-10-20 14:49:27 -0400

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
+from . import views
 from django.views.generic import TemplateView
 
-from . import views
-
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="index.html")),
     path("", include("allauth.urls")),
+    path('class/', views.get_class,name = "get_class"),
 ]
