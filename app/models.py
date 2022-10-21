4f59fc1b4e11772f9351c8e0c330b22e7076c129
Martin Crow <mec4ed@virginia.edu>
2022-10-21 17:00:19 -0400

    created course list page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models
+#
+# # Create your models here.
+# class UserClass(models.Model):
+#     description_field = models.CharField(max_length=50, default="description")
+#     course_number_field = models.CharField(max_length=10, default="course number")
+#     instructor_field = models.CharField(max_length=50, default="instructor")
+#     def __str__(self):
+#         return self.description_field
--- app/views.py
+++ app/views.py
@@ -2,12 +2,25 @@ from django.http import HttpResponse
 from http.client import responses
 from urllib import response
 from django.shortcuts import render
+# from .models import UserClass
 import requests
+from django.http import HttpResponseRedirect
 
 def index(request):
     return HttpResponse("Hello, world. You're at the app page.")
-    
+
 def get_class(request):
     url = "http://luthers-list.herokuapp.com/api/dept/CS/"
     response = requests.get(url).json()
     return render(request,'classinfo.html',{'response':response})
+
+# def submit(request):
+#     decsription_text = request.POST.get('Class Name')
+#     course_number_text = request.POST.get('Course Number')
+#     instructor_text = request.POST.get('Instructor')
+#     thought = UserClass(description_field=decsription_text, course_number_field=course_number_text, instructor_field=instructor_text)
+#     thought.save()
+#     # Always return an HttpResponseRedirect after successfully dealing
+#     # with POST data. This prevents data from being posted twice if a
+#     # user hits the Back button.
+#     return HttpResponseRedirect('/class')
