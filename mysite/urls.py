9b3d7bb466aa217be5343f24cd96dd8c8f3debed
zachna <112667032+zachna@users.noreply.github.com>
2022-10-22 19:29:46 -0400

    initial formatting to profile

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from app.views import profile
 
+urlpatterns = [
+    # Add this
+
+]
 urlpatterns = [
     path('', include('app.urls')),
     path('app/', include('app.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+    path('profile/', profile, name='users-profile'),
 ]
