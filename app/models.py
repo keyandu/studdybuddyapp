29a9aeaaa685228e225c905f440c8451f0220ed6
zachna <112667032+zachna@users.noreply.github.com>
2022-10-24 18:30:22 -0400

    zdn8kq patch 6
    ???????????????????

--- app/models.py
+++ app/models.py
@@ -14,8 +14,40 @@ class UserClass(models.Model):
 
 
 class Profile(models.Model):
+    Ages = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+        ('7', '7'),
+        ('8', '8'),
+        ('9', '9'),
+        ('10', '10'),
+        ('11', '11'),
+        ('12', '12'),
+        ('13', '13'),
+        ('14', '14'),
+        ('15', '15'),
+        ('16', '16'),
+        ('17', '17'),
+        ('18', '18'),
+        ('19', '19'),
+        ('20', '20'),
+        ('21', '21'),
+        ('22', '22'),
+        ('23', '23'),
+        ('24', '24'),
+        ('25', '25'),
+        ('26', '26'),
+        ('27', '27'),
+        ('28', '28'),
+        ('29', '29'),
+        ('30', '30'),
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    Age = models.TextField()
+    Age = models.CharField(max_length=150, choices=Ages)
     Enrolled_Courses = models.TextField()
     Major = models.TextField()
     Bio = models.TextField()
