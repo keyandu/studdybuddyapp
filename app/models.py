21f8ab6950dcdd1b16fa624aced4e1d2fb2b1052
keyandu <81390951+keyandu@users.noreply.github.com>
2022-11-05 17:35:41 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
-
+from django.dispatch import receiver
 
 #
 # # Create your models here.
@@ -51,6 +51,14 @@ class Profile(models.Model):
     Enrolled_Courses = models.TextField()
     Major = models.TextField()
     Bio = models.TextField()
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
     def __str__(self):
         return f'{self.user.username} Profile'
 
