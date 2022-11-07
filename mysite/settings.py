b793fb18d6881b63e849ab11dd9c0edd0964ae91
Keyan Du <kd7tw@virginia.edu>
2022-11-07 18:34:00 -0500

    c

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lvzuc)n7vre4v-34wqkz0((pg9u2bfh&daliipv9r-ep4gr7(^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','study-buddy-app2022.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','study-buddy-app2022.herokuapp.com','testserver']
 
 
 # Application definition
