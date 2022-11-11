657f0252bdbb6f39b0aa83254851656b35f01e95
Keyan Du <kd7tw@virginia.edu>
2022-11-11 14:13:28 -0500

    c

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from .views import StudySessionDetailView,AddSessionView,UpadateSessionView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   # path('', TemplateView.as_view(template_name="index.html"),name = "home"),
+    path('', TemplateView.as_view(template_name="index.html"),name = "home"),
     path('finder/', TemplateView.as_view(template_name="findBuddy.html"),name = "findBuddy"),
     path("", include("allauth.urls")),
     path('class/', views.get_class,name = "get_class"),
@@ -15,7 +15,7 @@ urlpatterns = [
     path('list/', views.post_list,name = 'list'),
     path('list/<int:pk>', StudySessionDetailView.as_view(), name = "post_detail"),
     path('enroll/<int:pk>', views.EnrollView, name = "enroll"),
-    path('',views.EnrolledSessionsView, name = "home"),
+    #path('',views.EnrolledSessionsView, name = "home"),
     path('list/edit/<int:pk>', UpadateSessionView.as_view(), name = "edit_session"),
     path('my_post_session/',views.ListMyPostSessions, name = "my_post_session"),
 
