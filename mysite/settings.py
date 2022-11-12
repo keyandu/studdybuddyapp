23339f4c5e39078446d988e7713e13b021afb246
Keyan Du <kd7tw@virginia.edu>
2022-11-12 14:53:17 -0500

    c

--- mysite/settings.py
+++ mysite/settings.py
@@ -167,8 +167,8 @@ SOCIALACCOUNT_PROVIDERS = {
                 ],
                 'AUTH_PARAMS': {
                     'access_type': 'offline',
-                    'redirect_uri': 'http://study-buddy-app2022.herokuapp.com/accounts/google/login/callback/',
-                    # 'redirect_uri': 'http://127.0.0.1:8000/accounts/google/login/callback/',
+                    #'redirect_uri': 'http://study-buddy-app2022.herokuapp.com/accounts/google/login/callback/',
+                     'redirect_uri': 'http://127.0.0.1:8000/accounts/google/login/callback/',
 
 
                 }
