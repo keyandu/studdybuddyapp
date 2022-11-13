16b35e70bed9040d75ce42358ff7a5815da09532
zachna <112667032+zachna@users.noreply.github.com>
2022-11-13 14:01:11 -0500

    fgdh
    deleted friends app

--- FriendsList/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-class friendslist(request):
-    profile = Profile.objects.get(user=request.user)
-    context = {'profile': profile}
-    return render(request, 'friendsList.html', context)
--- mysite/settings.py
+++ mysite/settings.py
@@ -45,9 +45,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    'friendship',
     'Chat',
-    'FriendsList',
+
     
 ]
 
